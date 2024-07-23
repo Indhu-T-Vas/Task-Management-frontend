c56411627b49b29da0d06bca4bd4945e9069318f
Indhu-T-Vas <indhuvas95@gmail.com>
2024-07-23 22:00:17 +0530

    taskfrontend

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import "./App.css";
 import SignUp from "./components/UserSection/signUp";
 import Login from "./components/UserSection/login";
 import Dashboard from "./components/dashboard";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
   const isLoggedIn = localStorage.getItem("loggedIn");
   return (
     <GoogleOAuthProvider clientId="849352318712-bvdsm30983kecdg352p548obdtr0hvcd.apps.googleusercontent.com">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route
-            path="/login"
-            element={isLoggedIn === true ? <Dashboard /> : <Login />}
-          />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/Task-Management-frontend" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/login"
+          element={isLoggedIn === true ? <Dashboard /> : <Login />}
+        />
+      </Routes>
     </GoogleOAuthProvider>
   );
 }
